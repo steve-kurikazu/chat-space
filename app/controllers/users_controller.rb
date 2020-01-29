8bf014fe5e3b68efddd353d7a6117e072bb78c55
steve-kurikazu <kazurin0202tab@gmail.com>
2020-01-29 15:21:28 +0900

    add-incremental-search-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
+  
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where('name LIKE ?', "%#{params[:keyword]}%" ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def edit
-  end
+  end  
 
   def update
     if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
-    end
-  end
+    end  
+  end  
 
   private
 
@@ -17,3 +26,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email)
   end
 end
+  
